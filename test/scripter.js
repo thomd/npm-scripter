76c26d9f42bab52ea8a8afac2ccac2eb5a64a7d8
Thomas Duerr <thomduerr@gmail.com>
2017-05-13 17:25:11 +0200

    refactored lib
    each npm script is now represented by a token-object with 'name' and
    'code'

--- test/scripter.js
+++ test/scripter.js
@@ -9,11 +9,16 @@ describe('npm-scripter', function() {
   var TEST_DIR, TEST_FILE
   var TEST_PKG = {
     "name": "scripter-test",
+    "version": "1.0.0",
     "scripts": {
       "foo": "npm run bar && npm run baz",
       "bar": "echo bar",
       "baz": "echo baz"
     }
+  }
+  var TEST_PKG_EMPTY = {
+    "name": "scripter-test",
+    "version": "1.0.0"
   }
 
   beforeEach(function(done) {
@@ -30,46 +35,56 @@ describe('npm-scripter', function() {
     done()
   })
 
-  it.skip('should find no npm scripts if no scripts are defined', function() {
-
+  it('should find no npm scripts if no scripts are defined', function() {
+    fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG_EMPTY))
+    var scripts = scripter.list(TEST_FILE)
+    assert.equal(scripts.length, 0)
   })
 
   it('should find all npm scripts', function() {
     fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG))
     var scripts = scripter.list(TEST_FILE)
-    assert.equal(Object.keys(scripts).length, 3)
+    assert.equal(scripts.length, 3)
+    assert.deepEqual(scripts.map(script => script.name), ["foo", "bar", "baz"])
   })
 
   it('should find npm script "foo"', function() {
     fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG))
-    var script = scripter.list(TEST_FILE, 'foo')
-    assert.equal(script, "npm run bar && npm run baz")
+    var scripts = scripter.list(TEST_FILE, 'foo')
+    assert.equal(scripts.length, 1)
+    assert.equal(scripts[0].name, "foo")
+    assert.equal(scripts[0].code, "npm run bar && npm run baz")
   })
 
-  it.skip('should add a first npm script "test"', function() {
-
+  it('should add a first npm script "test"', function() {
+    fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG_EMPTY))
+    scripter.add(TEST_FILE, 'test', 'echo test')
+    var scripts = scripter.list(TEST_FILE)
+    assert.equal(scripts.length, 1)
+    assert.equal(scripts[0].name, "test")
+    assert.equal(scripts[0].code, "echo test")
   })
 
   it('should add an additional npm script "test"', function() {
     fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG))
     scripter.add(TEST_FILE, 'test', 'echo test')
     var scripts = scripter.list(TEST_FILE)
-    assert.equal(Object.keys(scripts).length, 4)
-    var script = scripter.list(TEST_FILE, 'test')
-    assert.equal(script, 'echo test')
+    assert.equal(scripts.length, 4)
+    assert.deepEqual(scripts.map(script => script.name), ["foo", "bar", "baz", "test"])
   })
 
   it('should remove an existing npm script', function() {
     fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG))
-    scripter.remove(TEST_FILE, 'foo')
+    scripter.remove(TEST_FILE, 'bar')
     var scripts = scripter.list(TEST_FILE)
-    assert.equal(Object.keys(scripts).length, 2)
-    assert.equal(scripts['foo'], undefined)
-    assert.equal(scripts['bar'], "echo bar")
-    assert.equal(scripts['baz'], "echo baz")
+    assert.equal(scripts.length, 2)
+    assert.deepEqual(scripts.map(script => script.name), ["foo", "baz"])
   })
 
-  it.skip('should not remove a npm script if no scripts are defined', function() {
-
+  it('should not remove a npm script if no scripts are defined', function() {
+    fs.writeFileSync(TEST_FILE, JSON.stringify(TEST_PKG_EMPTY))
+    scripter.remove(TEST_FILE, 'bar')
+    var scripts = scripter.list(TEST_FILE)
+    assert.equal(scripts.length, 0)
   })
 })
