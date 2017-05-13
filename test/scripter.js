f5260e03f6dc501d708f77b35bb8d6b84e46fdb8
Thomas Duerr <thomduerr@gmail.com>
2017-05-13 12:47:52 +0200

    tests

--- test/scripter.js
+++ test/scripter.js
@@ -5,14 +5,14 @@ var path = require('path')
 var rimraf = require('rimraf')
 var scripter = require('../lib/scripter')
 
-describe('scripter', function() {
+describe('npm-scripter', function() {
   var TEST_DIR, TEST_FILE
   var TEST_PKG = {
     "name": "scripter-test",
     "scripts": {
       "foo": "npm run bar && npm run baz",
-      "bar": "scho bar",
-      "baz": "scho baz"
+      "bar": "echo bar",
+      "baz": "echo baz"
     }
   }
 
@@ -39,6 +39,7 @@ describe('scripter', function() {
   })
 
   it.skip('should find no npm scripts if no scripts are defined', function() {
+
   })
 
   it('should find all npm scripts', function() {
@@ -46,12 +47,13 @@ describe('scripter', function() {
     assert.equal(Object.keys(scripts).length, 3)
   })
 
-  it('should return npm script "foo"', function() {
+  it('should find npm script "foo"', function() {
     var script = scripter.list(TEST_FILE, 'foo')
     assert.equal(script, "npm run bar && npm run baz")
   })
 
   it.skip('should add a first npm script "test"', function() {
+
   })
 
   it('should add an additional npm script "test"', function() {
@@ -60,5 +62,18 @@ describe('scripter', function() {
     assert.equal(Object.keys(scripts).length, 4)
     var script = scripter.list(TEST_FILE, 'test')
     assert.equal(script, 'echo test')
+  })
+
+  it('should remove an existing npm script', function() {
+    scripter.remove(TEST_FILE, 'foo')
+    var scripts = scripter.list(TEST_FILE)
+    assert.equal(Object.keys(scripts).length, 2)
+    assert.equal(scripts['foo'], undefined)
+    assert.equal(scripts['bar'], "echo bar")
+    assert.equal(scripts['baz'], "echo baz")
+  })
+
+  it.skip('should not remove a npm script if no scripts are defined', function() {
+
   })
 })
